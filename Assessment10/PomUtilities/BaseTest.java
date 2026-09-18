dd990f6639f7030c35c23cfe9cb69745b62cb6ff
Vaibhav K <167295036+vaibhavraok@users.noreply.github.com>
2026-09-18 10:55:31 +0530

    Day 10

--- Assessment10/PomUtilities/BaseTest.java
+++ Assessment10/PomUtilities/BaseTest.java
@@ -18,7 +18,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
-
+//This is BaseClass
 public class BaseTest {
 	WebDriver driver = null;
 	protected Properties p = new Properties();
