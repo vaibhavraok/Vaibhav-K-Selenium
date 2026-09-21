3e6fb745fa0ea8e4c8672d6c08fa715a0a66805b
Vaibhav K <167295036+vaibhavraok@users.noreply.github.com>
2026-09-21 13:41:56 +0530

    Day 12

--- Assessment_12/pom/myinfopage.java
+++ Assessment_12/pom/myinfopage.java
@@ -1,76 +1,162 @@
+
 package Assessment12.pom;
 
-import java.util.List;
+import java.time.Duration;
 
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class myinfopage {
 
-	public WebDriver d;
-	public myinfopage(WebDriver d)
-	{
-		this.d = d;
-		PageFactory.initElements(d , this);
-	}
-	
-	@FindBy(name="firstName")
-	private WebElement FN;
-	
-	@FindBy(name="lastName")
-	private WebElement LN;
-	
-	@FindBy(xpath="//label[text()='Employee Id']/following::input[1]")
-	private WebElement EId;
-	
-	@FindBy(xpath="(//button[@type='submit'])[1]")
-	private WebElement savebtn;
-	
-	@FindBy(xpath="//p[text()='Vishusneha Bohaddurk']")
-	private WebElement verify;
-	
-	
-	public void getFN(String value) throws InterruptedException {
-		 FN.click();
-		 FN.sendKeys(Keys.CONTROL, "a");
-		 Thread.sleep(2000);
-		 FN.sendKeys(Keys.BACK_SPACE);
-		 FN.sendKeys(value);
-	}
+    public WebDriver d;
 
-	public void getLN(String value) throws InterruptedException {
-		 LN.click();
-		 LN.sendKeys(Keys.CONTROL, "a");
-		 Thread.sleep(2000);
-		 LN.sendKeys(Keys.BACK_SPACE);
-		 LN.sendKeys(value);
-	}
+    WebDriverWait wait;
 
-	public void getEId(String value) throws InterruptedException {
-		EId.click();
-		EId.sendKeys(Keys.CONTROL, "a");
-		Thread.sleep(2000);
-		EId.sendKeys(Keys.BACK_SPACE);
-		EId.sendKeys(value);
-	}
 
-	public void getSavebtn() {
-		savebtn.click();
-	}
-	
-	public String verifyFN() {
-		return FN.getAttribute("value");
-	}
-	
-	public String verifyLN() {
-		return LN.getAttribute("value");
-	}
+    public myinfopage(WebDriver d) {
 
-	public String verifyEId() {
-		return EId.getAttribute("value");
-	}
-	
+        this.d = d;
+
+        PageFactory.initElements(d, this);
+
+        wait = new WebDriverWait(d, Duration.ofSeconds(15));
+
+    }
+
+
+    @FindBy(name = "firstName")
+    private WebElement FN;
+
+
+    @FindBy(name = "lastName")
+    private WebElement LN;
+
+
+    @FindBy(xpath = "//label[text()='Employee Id']/following::input[1]")
+    private WebElement EId;
+
+
+    @FindBy(xpath = "(//button[@type='submit'])[1]")
+    private WebElement savebtn;
+
+
+    // Wait until form loader disappears
+
+    public void waitForLoader() {
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(
+                By.cssSelector(".oxd-form-loader")));
+
+    }
+
+
+    // First Name
+
+    public void getFN(String value) {
+
+        waitForLoader();
+
+        wait.until(ExpectedConditions.elementToBeClickable(FN));
+
+        FN.click();
+
+        FN.sendKeys(Keys.CONTROL, "a");
+
+        FN.sendKeys(Keys.BACK_SPACE);
+
+        FN.sendKeys(value);
+
+    }
+
+
+    // Last Name
+
+    public void getLN(String value) {
+
+        waitForLoader();
+
+        wait.until(ExpectedConditions.elementToBeClickable(LN));
+
+        LN.click();
+
+        LN.sendKeys(Keys.CONTROL, "a");
+
+        LN.sendKeys(Keys.BACK_SPACE);
+
+        LN.sendKeys(value);
+
+    }
+
+
+    // Employee ID
+
+    public void getEId(String value) {
+
+        waitForLoader();
+
+        wait.until(ExpectedConditions.elementToBeClickable(EId));
+
+        EId.click();
+
+        EId.sendKeys(Keys.CONTROL, "a");
+
+        EId.sendKeys(Keys.BACK_SPACE);
+
+        EId.sendKeys(value);
+
+    }
+
+
+    // Save button
+
+    public void getSavebtn() {
+
+        waitForLoader();
+
+        wait.until(ExpectedConditions.elementToBeClickable(savebtn));
+
+        savebtn.click();
+
+    }
+
+
+    // Verify First Name
+
+    public String verifyFN() {
+
+        waitForLoader();
+
+        return FN.getAttribute("value");
+
+    }
+
+
+    // Verify Last Name
+
+    public String verifyLN() {
+
+        waitForLoader();
+
+        return LN.getAttribute("value");
+
+    }
+
+
+    // Verify Employee ID
+
+    public String verifyEId() {
+
+        waitForLoader();
+
+        return EId.getAttribute("value");
+
+    }
+
 }
