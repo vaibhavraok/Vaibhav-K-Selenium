33815d97cdc50a031d11fdc314921688327e95d5
Vaibhav K <167295036+vaibhavraok@users.noreply.github.com>
2026-09-22 20:07:01 +0530

    Update PropertyUtility.java
    Refactored PropertyUtility class to load properties from a new file path and added file closing.

--- Final_Assessment/Advance_Assessment/utilities/PropertyUtility.java
+++ Final_Assessment/Advance_Assessment/utilities/PropertyUtility.java
@@ -1,15 +1,23 @@
-package Assessments.LastDayTask2.utilities;
+package utility;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyUtility {
+
     public static String getData(String key) throws IOException {
-        FileInputStream file=new FileInputStream("src/test/java/Assessments/LastDayTask2/resources/common_data.properties");
-        Properties properties=new Properties();
-        properties.load(file);
-        String data=properties.getProperty(key);
-        return key;
+
+        FileInputStream fis = new FileInputStream("./src/test/resources/DDT/common_data_OHRM.properties");
+
+        Properties p = new Properties();
+
+        p.load(fis);
+
+        String value = p.getProperty(key);
+
+        fis.close();
+
+        return value;
     }
 }
