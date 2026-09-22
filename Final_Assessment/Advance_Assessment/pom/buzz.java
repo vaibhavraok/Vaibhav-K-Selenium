790b9cce30bcb882e724dd23373e678f1a8ae6e1
Vaibhav K <167295036+vaibhavraok@users.noreply.github.com>
2026-09-22 20:08:04 +0530

    Final Assessment

--- Final_Assessment/Advance_Assessment/pom/buzz.java
+++ Final_Assessment/Advance_Assessment/pom/buzz.java
@@ -1,52 +1,44 @@
 package orange_HRM;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utility.ExcelUtility;
+
 public class buzz {
 
-	WebDriver d;
-	public buzz(WebDriver d)
-	{
-		this.d = d;
-		PageFactory.initElements(d , this);
-	}
-	
-	@FindBy(xpath="//textarea[@class='oxd-buzz-post-input']")
-	private WebElement textfield;
-	
-	@FindBy(xpath="//button[@type='submit']")
-	private WebElement postbtn;
-	
-	@FindBy(xpath ="//button[@class='oxd-button oxd-button--medium oxd-button--label-warn orangehrm-post-filters-button']/../..//p[@class='oxd-text oxd-text--p orangehrm-buzz-post-body-text']")
-	private WebElement verify;
-	
-	public void getTextfield() throws IOException {
-		//fetching data from excel
-		FileInputStream fis = new FileInputStream("./src/test/resources/DDT/data_OHRM.xlsx");
-		Workbook wb = WorkbookFactory.create(fis);
-		Sheet sh = wb.getSheet("Sheet1");
-		Row row = sh.getRow(1);	
-		String text = row.getCell(0).getStringCellValue();
-		textfield.sendKeys(text);
-	}
-	
-	public void getPostbtn() {
-		postbtn.click();
-	}
-	
-	public boolean getVerify() {
-		return verify.isDisplayed();
-	}
-	
+    WebDriver d;
+
+    public buzz(WebDriver d) {
+        this.d = d;
+        PageFactory.initElements(d, this);
+    }
+
+    @FindBy(xpath="//textarea[@class='oxd-buzz-post-input']")
+    private WebElement textfield;
+
+    @FindBy(xpath="//button[@type='submit']")
+    private WebElement postbtn;
+
+    @FindBy(xpath="//button[@class='oxd-button oxd-button--medium oxd-button--label-warn orangehrm-post-filters-button']/../..//p[@class='oxd-text oxd-text--p orangehrm-buzz-post-body-text']")
+    private WebElement verify;
+
+    public void getTextfield() throws IOException {
+
+        String text = ExcelUtility.getData("sheet2", 1, 0);
+
+        textfield.sendKeys(text);
+    }
+
+    public void getPostbtn() {
+        postbtn.click();
+    }
+
+    public boolean getVerify() {
+        return verify.isDisplayed();
+    }
 }
